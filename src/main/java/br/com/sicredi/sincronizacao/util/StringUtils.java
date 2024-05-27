4fee790b4dd511303502a33ba540126833f6fa3a
Carlos-Quest <carlos.costa.quest@gmail.com>
2024-05-27 01:30:32 -0300

    Inclusão do File.separator para execução em qualquer sistema operacional
    Pelo fato do desenvolvimento e testes terem sido realizados em ambiente Linux, inicialmente não foi identificado nenhum problema, mas quando realizei testes no ambiente Windows, verifiquei que se tornou necessário incluir o File.separator

--- src/main/java/br/com/sicredi/sincronizacao/util/StringUtils.java
+++ src/main/java/br/com/sicredi/sincronizacao/util/StringUtils.java
@@ -1,12 +1,14 @@
 package br.com.sicredi.sincronizacao.util;
 
+import java.io.File;
+
 public class StringUtils {
 
   private StringUtils() {
   }
 
   private static String getDirectoryPath(String filePath) {
-    Integer lastindex = filePath.lastIndexOf("/");
+    Integer lastindex = filePath.lastIndexOf(File.separator);
     return filePath.substring(0, lastindex);
   }
 
